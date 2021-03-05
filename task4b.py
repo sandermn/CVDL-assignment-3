30abe2a29f398d8102ea1c6636c3171985c49c01
Olav Meberg <56315637+olavrm@users.noreply.github.com>
2021-03-05 11:24:13 +0100

    Update task4b.py
    added 4b vizz

--- task4b.py
+++ task4b.py
@@ -45,5 +45,17 @@ def torch_image_to_numpy(image: torch.Tensor):
     image = np.moveaxis(image, 0, 2)
     return image
 
+''''
+task 4b
+''''
 
 indices = [14, 26, 32, 49, 52]
+plt.figure(figsize=(20, 8))
+L = len(indices)
+for i in range(L):
+    image = torch_image_to_numpy(activation[0][indices[index]])
+    plt.subplot(1, 5, i+1)
+    plt.imshow(image)
+
+plt.show()
+plt.savefig('Visualize_pic.png', format ="png")
